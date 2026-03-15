1f9aca0f544044386aed2d2ce5b68dab58065ca4
rahman2220510189 <rahman22205101894@diu.edu.bd>
2026-03-15 16:43:58 +0600

    this is the last commit for my projects

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://career-coach-backend-system.onrender.com/api",
 });
 
 // Add token to every request automatically
